e0a549754b8a56d880825007a0a189d50683c1b6
VINICIUS CALADO <vinicius.cl.dev@gmail.com>
2024-08-26 14:27:16 -0300

    V1.01 comentarios removidos

--- sistema-de-cinema/biblioteca.c
+++ sistema-de-cinema/biblioteca.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-#define QTD 50 // Defina o valor de qtd aqui
-#define MAX_ID 99 // define quantos ids de filmes podem existir
+#define QTD 50 
+#define MAX_ID 99
 
 int main(){
-	int opc=0;
-	int n=0;
+	int opc = 0;
+	int n = 0;
 
 	int id[QTD] = {0}, anoLancamento[QTD] = {0};
-	char titulo[QTD][50], sinopse [QTD][255], genero[QTD][50];
+	char titulo[MAX_ID][50], sinopse [MAX_ID][255], genero[MAX_ID][50];
 
 	while(opc != 5)
 	{
@@ -22,7 +22,7 @@ int main(){
 		scanf("%d", &opc);
 		printf("\n");
 
-		if(opc==1)
+		if( opc == 1 )
 		{
 			for(int i = 1; i <= 99; i++){
 				if( id[i] == 0) {
@@ -38,39 +38,32 @@ int main(){
 			printf("Informações sobre o filme\n");
 			printf("Digite o Titulo: ");
 			getchar(); //limpar o buffer
-			fgets(titulo[n], 50, stdin);
-			//scanf("%[^\n]", titulo[n]);
+			fgets(titulo[n], QTD, stdin);
 			printf("Digite a Sinopse: ");
 			fgets(sinopse[n], 50, stdin);
-			//scanf("%[^\n]", sinopse[n]);
+			printf("Digite o genero: ");
+			fgets(genero[n], 50, stdin);
 			printf("Digite o ano de Lançamento: ");
 			scanf("%d", &anoLancamento[n]);
-			printf("CADASTRADO COM SUCESSO!!!\n");
+			printf("CADASTRADO COM SUCESSO!!!\n\n");
 			
 			opc=0; //retorna ao menu
 		}
 
-		if(opc==2)
-		{
-			//MODIFICAR
-			//DIGITE O ID DO FILME A SER EDITADO:
-			  //verifica se o id existe, se está no intervalo dos filmes criados.
-			//EXIBIR AS INFORMAÇÕES ATUAIS DO FILME
-			//APOS ISSO INICIA A EDIÇÃO
-			
-
+		if( opc == 2 )
+		{			
 			printf("Digite o id do Filme para EDITAR: ");
 			scanf("%d", &n);
 			printf("\n");
 
-			for(int i = 1; i <= MAX_ID; i++)
+			for( int i = 1; i <= MAX_ID; i++ )
 			{
 				if( ( id[i] != 0 ) && ( id[i] == n ) )
 				{
-					//exibe info
 					printf("EXIBINDO INFORMAÇÕES ATUAIS.\n");
 					printf("TITULO : %s", titulo[n]);
-					printf("GENERO : %s", sinopse[n]);
+					printf("SINOPSE : %s", sinopse[n]);
+					printf("GENERO : %s", genero[n]);
 					printf("ANO : %d", anoLancamento[n]);
 
 					printf("\n");
@@ -78,15 +71,15 @@ int main(){
 					printf("DIGITE AS NOVAS INFORMAÇÕES\n");
 					getchar(); //limpar o buffer
 					printf("TITULO: ");
-					fgets(titulo[n], 50, stdin);
+					fgets(titulo[n], QTD, stdin);
 					printf("SINOPSE: ");
-					fgets(sinopse[n], 50, stdin);
+					fgets(sinopse[n], QTD, stdin);
 					printf("GENERO: ");
-					fgets(genero[n], 50 , stdin);
+					fgets(genero[n], QTD , stdin);
 					printf("ANO: ");
 					scanf("%d", &anoLancamento);
 					
-					printf("INFORMAÇÕES MODIFICADAS\n");
+					printf("INFORMAÇÕES MODIFICADAS COM SUCESSO\n");
 					break;
 				}
 			}
@@ -96,36 +89,23 @@ int main(){
 
 		if(opc==3)
 		{
-			//como deletar???
-			//vai movendo as informações
-            //recebe o id
-            //primeiro verificar se o id existe,com um for que vai do id escolhido até o max, e verifica se o id != 0.
-
-
-           //a partir dela vai movedando o da direita para esquerda ou seja, em um while enquanto id != 0
-           //titulo[remover] = titulo[substituo], id=[remover] = id[substituto]
            printf("DIGITE O ID DO FILME A SER DELETADO!");
            scanf("%d", &n);
 
            if ( ( n <= MAX_ID ) && ( n != 0 ) && id[n] != 0 ) //se satisfaz VAI existir um filme nesse id! 
              {
-                //logica ir movendo do maior id para o menor. até que encontre um id=0
                while (n != 0)
                 {
-					//se o próximo id for 0, ou seja o fim, encerra o while.
                     if( id[n+1] == 0 )  
                     {
                     	id[n]=0;
                     	printf("EXCLUSAO CONCLUIDA");
                     	break;
                     }
-                	//id deve permanecer o mesmo, oque muda são as informações
-                    //titulo[n] = titulo[n+1];
-                    //sinopse[n] = sinopse[n+1];
                     anoLancamento[n] = anoLancamento[n+1];
                     strcpy(titulo[n], titulo[n + 1]);
                     strcpy(sinopse[n], sinopse[n + 1]);
-                    //anoLancamento[j] = anoLancamento[j + 1];
+                    strcpy(genero[n], genero[n + 1]);
                     n++;
                 }
              }
@@ -140,21 +120,23 @@ int main(){
 		if(opc==4)
 		{
 			printf("===TODOS OS FILMES===\n");
-			
-			//se não houver nenhum filme alertar
-			for(int i = 1; i <= MAX_ID; i++){
+			for(int i = 1; i <= MAX_ID; i++)
+			{
 				if( id[i] == 0 ){
-					printf("\nTODOS OS FILMES FORAM EXIBIDOS!\n\n");
+					printf("TODOS OS FILMES FORAM EXIBIDOS!\n\n");
 					break;
 				}
 				printf("%d.%s", i, titulo[i] );
+				printf("SINOPSE: %s", sinopse[i]);
+				printf("GENERO: %s", genero[i]);
+				printf("ANO DE LANÇAMENTO: %d\n\n", anoLancamento[i]);
 			}
 			opc=0;
 		}
 
 		if(opc==5)
 		{
-			printf("ENCERRADO\n");
+			printf("PROGRAMA ENCERRADO!!!\n");
 			break;
 		}
 
